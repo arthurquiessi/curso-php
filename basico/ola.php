b9a35aa3a3bc0f836dc282aa2cef1ffbeaec3037
arthurquiessi <arthur.bianchi.quiessi@gmail.com>
2020-10-13 16:45:39 -0300

    psc

---
+++ basico/ola.php
@@ -0,0 +1,17 @@
+<div class="titulo">Olá PHP</div>
+
+<h2>Blocos PHP</h2>
+
+<h2>Exemplo A</h2>
+<?php 
+    echo 'Olá '; 
+    echo "Mundo! <br>" // \n quebra linha por padrão no PHP, com HTML usa-se <br>
+?>
+
+<h2>Exemplo B</h2>
+<?= 
+ "Outra forma de me expressar"
+?>
+
+<?php
+phpinfo();
---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="assets/css/estilo.css">
+    <title>Curso PHP</title>
+</head>
+
+<body>
+    <header class="cabecalho">
+        <h1>Curso PHP</h1>
+        <h2>Índice dos Exercícios</h2>
+    </header>
+    <main class="principal">
+        <div class="conteudo">
+            <nav class="modulos">
+                <div class="modulo verde">
+                    <h3>Módulo 01</h3>
+                    <ul>
+                        <li><a href="exercicio.php?dir=basico&file=ola">Olá PHP</a></li>
+                    </ul>
+                </div>
+            </nav>
+        </div>
+    </main>
+    <footer class="rodape">
+        ALPINO & T.I @ <?= date('Y'); ?>
+    </footer>
+</body>
+
+</html>
